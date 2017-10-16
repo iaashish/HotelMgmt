6ba9ed7eee684bc1645b7bcc463c7e7644156087
iaashish <ippiliaashish@gmail.com>
2017-10-16 16:45:18 -0500

    Initial code commit

---
+++ hotelmanagement/resources/views/welcome.blade.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Laravel</title>
+      <!-- Fonts -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+      <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+      <!-- Styles -->
+      <style>
+      ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111;
+}
+
+         html, body {
+         background-color: #999999;
+         color: #636b6f;
+         font-family: 'Raleway', sans-serif;
+         font-weight: 100;
+         height: 100vh;
+         margin: 0;
+         }
+         .full-height {
+         height: 100vh;
+         }
+         .flex-center {
+         align-items: center;
+         display: flex;
+         justify-content: center;
+         }
+         .position-ref {
+         position: relative;
+         }
+         .top-right {
+         right: 10px
+         top: 18px;
+         }
+         .content {
+         text-align: center;
+         }
+         .title {
+         font-size: 84px;
+         }
+         .links > a {
+         color: #636b6f;
+         padding: 0 25px;
+         font-size: 12px;
+         font-weight: 600;
+         letter-spacing: .1rem;
+         text-decoration: none;
+         text-transform: uppercase;
+         }
+         .m-b-md {
+         margin-bottom: 30px;
+         }
+      </style>
+   </head>
+   <body>
+     
+             
+
+            <div class="top-right links">
+               <ul>
+               <li><a href="/home">Home</a></li>
+               <li><a href="/login">Contact us</a></li>
+               <li><a href="/login">About us</a></li>
+               <ul>
+            </div>
+         
+          <div class="flex-center position-ref full-height" >
+         <div class="content">
+            <div class="title m-b-md">
+               <a href="/managerlogin">Manager Login</a>
+               <a href="/managerlogin">Staff Login</a>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
