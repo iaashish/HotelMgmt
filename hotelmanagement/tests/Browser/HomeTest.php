ae0897b68ed1de6ad5f29743c68e6ccba7eebb99
Brennan Guidry <Brennan>
2017-10-31 15:13:36 -0500

    Added some more beef to the test cases

--- hotelmanagement/tests/Browser/HomeTest.php
+++ hotelmanagement/tests/Browser/HomeTest.php
@@ -19,7 +19,14 @@ class HomeTest extends DuskTestCase
             $browser->visit('/')
                     ->assertSee('Hotel Management System')
                     ->assertSeeLink('Manager Login')
-                    ->assertSeeLink('Staff Login');
+                    ->assertSeeLink('Staff Login')
+                    ->assertSeeLink('Admin Login')
+                    ->assertSeeLink('HOME')
+                    ->assertSeeLink('REGISTRATION')
+                    ->assertSelected('ABOUT US', 'Brennan Guidry')
+                    ->assertSelected('ABOUT US', 'Ippili Aashish')
+                    ->assertSelected('ABOUT US', 'Oyetokunbo Ipaye')
+                    ->assertSelected('ABOUT US', 'Taghreed Alghamdi');
         });
     }
 }
