ad1ca36bbfaf776c16f160eef8812b5372160ded
Brennan Guidry <Brennan>
2017-11-12 12:14:26 -0600

    another browser test

---
+++ hotelmanagement/tests/Browser/IncorrectLoginManagerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
+
+class IncorrectLoginManagerTest extends DuskTestCase
+{
+    public function testManagerLogin()
+    {
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/')
+        ->clickLink('Manager Login')
+        ->assertPathIs('/login')
+        ->value('#email','wrongEmail')
+        ->assertInputValue('#email', 'wrongEmail')
+        ->value('#password','!@#$%')
+        ->assertInputValue('#password', '!@#$%')
+        ->press('Login')
+        //incorrect login because of invalid email type
+        ->assertPathIs('/login');
+    });
+
+    }
+
+}
