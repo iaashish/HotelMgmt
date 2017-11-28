68262d6f661f4e024ed77375d6de09c587b0fdad
Brennan Guidry <Brennan>
2017-11-28 17:47:55 -0600

    fixed user seed table

--- hotelmanagement/database/seeds/DatabaseSeeder.php
+++ hotelmanagement/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        $this->call([UsersTableSeeder::class,StaffTableSeeder::class]);
 
 //         DB::table('users')->insert([
 //            'name' => str_random(10),
